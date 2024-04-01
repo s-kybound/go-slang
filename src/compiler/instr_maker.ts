46f2802cfbb5cca278e546916cda211943473685
s-kybound <abadkyriel@gmail.com>
2024-04-02 02:35:56 +0800

    make instructions for go

---
+++ src/compiler/instr_maker.ts
@@ -0,0 +1,76 @@
+import {
+  InstrType,
+  UnopType,
+  BinopType,
+  Instr,
+  LDCInstr,
+  UNOPInstr,
+  BINOPInstr,
+  POPInstr,
+  JOFInstr,
+  GOTOInstr,
+  ENTER_SCOPEInstr,
+  EXIT_SCOPEInstr,
+  LDInstr,
+  ASSIGNInstr,
+  LDFInstr,
+  CALLInstr,
+  TCALLInstr,
+  RESETInstr
+} from "./instructions"
+
+export function makeLDCInstr(value: any): LDCInstr {
+  return { type: InstrType.LDC, value: value }
+}
+
+export function makeUNOPInstr(op: UnopType): UNOPInstr {
+  return { type: InstrType.UNOP, op: op }
+}
+
+export function makeBINOPInstr(op: BinopType): BINOPInstr {
+  return { type: InstrType.BINOP, op: op }
+}
+
+export function makePOPInstr(): POPInstr {
+  return { type: InstrType.POP }
+}
+
+export function makeJOFInstr(addr: number): JOFInstr {
+  return { type: InstrType.JOF, addr: addr }
+}
+
+export function makeGOTOInstr(addr: number): GOTOInstr {
+  return { type: InstrType.GOTO, addr: addr }
+}
+
+export function makeENTER_SCOPEInstr(): ENTER_SCOPEInstr {
+  return { type: InstrType.ENTER_SCOPE }
+}
+
+export function makeEXIT_SCOPEInstr(): EXIT_SCOPEInstr {
+  return { type: InstrType.EXIT_SCOPE }
+}
+
+export function makeLDInstr(name: string): LDInstr {
+  return { type: InstrType.LD, name: name }
+}
+
+export function makeASSIGNInstr(name: string): ASSIGNInstr {
+  return { type: InstrType.ASSIGN, name: name }
+}
+
+export function makeLDFInstr(params: any[], addr: number): LDFInstr {
+  return { type: InstrType.LDF, params: params, addr: addr }
+}
+
+export function makeCALLInstr(arity: number): CALLInstr {
+  return { type: InstrType.CALL, arity: arity }
+}
+
+export function makeTCALLInstr(arity: number): TCALLInstr {
+  return { type: InstrType.TCALL, arity: arity }
+}
+
+export function makeRESETInstr(): RESETInstr {
+  return { type: InstrType.RESET }
+}
--- src/compiler/instructions.ts
+++ src/compiler/instructions.ts
@@ -1,3 +1,110 @@
 /** 
  * This file contains the instruction set of our compiler.
  */
+
+export enum InstrType {
+  LDC,
+  UNOP,
+  BINOP,
+  POP,
+  JOF,
+  GOTO,
+  ENTER_SCOPE, // or env?
+  EXIT_SCOPE,
+  LD,
+  ASSIGN,
+  LDF,
+  CALL,
+  TCALL,
+  RESET
+}
+
+export enum UnopType {
+  NEG,
+  NOT
+}
+
+export enum BinopType {
+  ADD,
+  SUB,
+  MUL,
+  DIV,
+  MOD,
+  AND,
+  OR,
+  EQ,
+  NEQ,
+  LT,
+  GT,
+  LE,
+  GE
+}
+
+export interface BaseInstr {
+  type: InstrType;
+}
+
+export interface LDCInstr extends BaseInstr {
+  value: any;
+}
+
+export interface UNOPInstr extends BaseInstr {
+  op: UnopType;
+}
+
+export interface BINOPInstr extends BaseInstr {
+  op: BinopType;
+}
+
+export interface POPInstr extends BaseInstr {}
+
+export interface JOFInstr extends BaseInstr {
+  addr: number;
+}
+
+export interface GOTOInstr extends BaseInstr {
+  addr: number;
+}
+
+export interface ENTER_SCOPEInstr extends BaseInstr {}
+
+export interface EXIT_SCOPEInstr extends BaseInstr {}
+
+export interface LDInstr extends BaseInstr {
+  name: string;
+}
+
+export interface ASSIGNInstr extends BaseInstr {
+  name: string;
+}
+
+export interface LDFInstr extends BaseInstr {
+  params: any[];
+  addr: number;
+}
+
+export interface CALLInstr extends BaseInstr {
+  arity: number;
+}
+
+export interface TCALLInstr extends BaseInstr {
+  arity: number;
+}
+
+export interface RESETInstr extends BaseInstr {}
+
+export type Instr = 
+  | LDCInstr 
+  | UNOPInstr 
+  | BINOPInstr 
+  | POPInstr 
+  | JOFInstr 
+  | GOTOInstr 
+  | ENTER_SCOPEInstr 
+  | EXIT_SCOPEInstr 
+  | LDInstr 
+  | ASSIGNInstr 
+  | LDFInstr 
+  | CALLInstr 
+  | TCALLInstr 
+  | RESETInstr;
