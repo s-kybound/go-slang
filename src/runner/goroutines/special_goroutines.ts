2b2d6e95bf640b7ae4131329c0989cc5d83ac66e
s-kybound <abadkyriel@gmail.com>
2024-04-03 23:10:25 +0800

    keep things compilable

--- src/runner/goroutines/special_goroutines.ts
+++ src/runner/goroutines/special_goroutines.ts
@@ -1,5 +1,5 @@
 import { Goroutine } from "./goroutine";
 
-export class GarbageCollector implements Goroutine {
+export class GarbageCollector /*implements Goroutine*/ {
   // currently placeholder as we don't have memory management
 }
