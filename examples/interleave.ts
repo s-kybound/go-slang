e1d5e978910012870b9b1a2c047efcfeb82bd2a6
joytqt-1202 <96771931+joytqt-1202@users.noreply.github.com>
2024-04-21 00:32:43 +0800

    Update interleave.ts

--- examples/interleave.ts
+++ examples/interleave.ts
@@ -1,17 +1,3 @@
-func add(x, y number) number {
-  return x + y;
-}
-
-func give_three() (number, number, number) {
-  return 4, 5, 6;
-}
-
-func check(x, y, z number) {
-  display(x);
-  display(y);
-  display(z);
-}
-
 func async1() {
   display("async1 running:");
   for x := 0; x < 100; x = x + 1 {
@@ -30,16 +16,6 @@ func async2(c chan number) {
 }
 
 func main() {
-  d := make_channel();
-  func inner() {
-    display("hello");
-  }
-  x, y, z := 1, 2, 3;
-  check(x, y, z);
-  x, y, z = give_three();
-  check(x, y, z);
-  check(a, b, c);
-  inner();
   go async1();
   go async2(d);
   display("main running:");
